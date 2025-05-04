422152003e1677806197bb35e053e9b394e51814
Mahender Kumar <mksb1947@gmail.com>
2025-05-05 01:13:56 +0530

    Add Hilt dependencies for dependency injection in Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,11 @@ dependencies {
     implementation ("de.undercouch:gradle-download-task:4.1.2")
 
     implementation(libs.androidx.hilt.navigation.fragment)
+    implementation ("com.google.dagger:hilt-android:2.48")
+    ksp ("com.google.dagger:hilt-compiler:2.48")
+
+    // Hilt for Jetpack Compose
+    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
 
 
 
